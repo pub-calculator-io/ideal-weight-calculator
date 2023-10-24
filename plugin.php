fadb3eeb734bba5e1d5db11de0c64ce6be01556c
Arseniy Olevskiy <arsidian@gmail.com>
2023-10-24 15:54:16 +0400

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,6 +10,8 @@ License: GPLv2 or later
 Text Domain: ci_ideal_weight_calculator
 */
 
+if (!defined('ABSPATH')) exit;
+
 if (!function_exists('add_shortcode')) return "No direct call for Ideal Weight Calculator by Calculator.iO";
 
 function display_ci_ideal_weight_calculator(){
